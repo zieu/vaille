6f676cbd17a1e3dab008563b23a750d72528a74b
Zieu <zieu2000@gmail.com>
2021-04-27 09:50:17 +0500

    added page title

--- src/components/shared/layout/layout.tsx
+++ src/components/shared/layout/layout.tsx
@@ -4,13 +4,19 @@ import { Left, Right } from ".";
 
 type Props = {
 	children?: React.FC | React.ReactNode;
+	pageTitle: string;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ children, pageTitle }: Props) => {
 	return (
 		<div className="flex">
 			<Left />
-			<div className="bg-light-grayish dark:bg-dark-off w-full">{children}</div>
+			<div className="bg-light-grayish dark:bg-dark-off w-full">
+				<h1 className="text-white bg-dark-deep py-1 border-b-4 pl-6 text-lg font-bold border-dark-lie">
+					{pageTitle ? pageTitle : <span className="font-thin text-gray-500">Loading...</span>}
+				</h1>
+				{children}
+			</div>
 			<Right />
 		</div>
 	);
