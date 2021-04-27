25a669a91e0b11091cc4e77eac33ad9728a11c3c
Zieu <zieu2000@gmail.com>
2021-04-27 14:40:09 +0500

    post card partially done

--- src/components/shared/post-card.tsx
+++ src/components/shared/post-card.tsx
@@ -4,7 +4,7 @@ type Props = PostDTO;
 
 const PostCard = ({ title, user, content, image }: Props) => {
 	return (
-		<div className="bg-dark-cloud w-96 p-4 rounded-lg mb-4">
+		<div className="bg-dark-cloud w-96 p-4 rounded-lg mb-3">
 			<h2 className="font-bold text-xl mb-2">{title}</h2>
 			<p>{content}</p>
 		</div>
