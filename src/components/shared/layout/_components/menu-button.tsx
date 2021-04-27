f0f243cf078ee6e7d75be8b49f62c8f0e03e945a
Zieu <zieu2000@gmail.com>
2021-04-27 21:45:54 +0500

    menu light theme version is done

--- src/components/shared/layout/_components/menu-button.tsx
+++ src/components/shared/layout/_components/menu-button.tsx
@@ -1,6 +1,7 @@
 import cn from "classnames";
 import { Url } from "node:url";
 
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -14,16 +15,24 @@ type Props = {
 
 const MenuButton = ({ link, label, icon }: Props) => {
 	const { pathname } = useRouter();
+	const { theme } = useTheme();
 	const active = pathname === link;
 	return (
 		<Link href={link!}>
 			<div
-				className={cn("hover:bg-dark-cloud py-3 px-5 text-lg mb-2 font-semibold rounded-xl cursor-pointer", {
-					"menu-button--active": active,
-				})}>
+				className={cn(
+					"hover:bg-light-grayish dark:text-white text-light-text dark:hover:bg-dark-cloud py-3 px-5 text-lg mb-2 font-semibold rounded-xl cursor-pointer",
+					{
+						"menu-button--active menu-button--active--light": active && theme === "light",
+						"menu-button--active menu-button--active--dark": active && theme === "dark",
+					},
+				)}>
 				<a>
 					<div className="flex items-center">
-						<span>{active && icon ? icon[1] : icon && !active && icon[0]}</span> <span className="ml-4">{label}</span>
+						<span className={cn({ "menu-icon--light": theme === "light" })}>
+							{active && icon ? icon[1] : icon && !active && icon[0]}
+						</span>{" "}
+						<span className="ml-4">{label}</span>
 					</div>
 				</a>
 			</div>
