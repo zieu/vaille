49dece6b0aceecacc6dd1354678d03bf161dcb06
Zieu <zieu2000@gmail.com>
2021-04-27 21:12:23 +0500

    made things little-bit smaller

--- src/components/shared/post-card.tsx
+++ src/components/shared/post-card.tsx
@@ -9,8 +9,8 @@ const PostCard = ({ title, user, content, image }: Props) => {
 		<div>
 			<User {...user} />
 			<div className="bg-dark-cloud w-96 p-4 rounded-lg mb-4">
-				<h2 className="font-bold text-xl mb-2">{title}</h2>
-				<p>{content}</p>
+				<h2 className="font-bold text-lg mb-2">{title}</h2>
+				<p className="text-sm">{content}</p>
 			</div>
 		</div>
 	);
--- src/components/shared/user.tsx
+++ src/components/shared/user.tsx
@@ -10,18 +10,18 @@ const User = ({ avatar, isFollowed, name }: Props) => {
 			<div className="flex items-center">
 				<img
 					src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
-					width="50px"
-					height="50px"
+					width="40px"
+					height="40px"
 					className="rounded-full border-2 mr-2"
 				/>
-				<span className="text-lg">{name}</span>
+				<span className="font-bold">{name}</span>
 			</div>
 			{isFollowed ? (
-				<button className="bg-primary-purple border-primary-purple rounded-lg px-3 py-1 font-bold focus:outline-none focus:ring-2">
+				<button className="bg-primary-purple text-sm border-primary-purple rounded-lg px-2 py-1 font-bold focus:outline-none focus:ring-2">
 					Following
 				</button>
 			) : (
-				<button className="border rounded-lg px-3 py-1 font-bold focus:outline-none focus:ring-2">Follow</button>
+				<button className="border rounded-lg px-2	text-sm py-1 font-bold focus:outline-none focus:ring-2">Follow</button>
 			)}
 		</div>
 	);
