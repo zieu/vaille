36fcc8aee0a121001bd1c9e644565758b8deb6b5
Zieu <zieu2000@gmail.com>
2021-05-05 22:08:52 +0500

    impelemented raw auth

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,21 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_ID?: string;
+    GITHUB_SECRET?: string;
+    TWITTER_ID?: string;
+    TWITTER_SECRET?: string;
+    AUTH0_CLIENT_ID?: string;
+    AUTH0_CLIENT_SECRET?: string;
+    AUTH0_DOMAIN?: string;
+    EMAIL_SERVER_USER?: string;
+    EMAIL_SERVER_PASSWORD?: string;
+    EMAIL_SERVER_HOST?: string;
+    EMAIL_SERVER_PORT?: string;
+    EMAIL_FROM?: string;
+    NEXTAUTH_URL?: string;
+    DATABASE_URL?: string;
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { withLayout } from "containers";
 
+import { signIn, signOut, useSession } from "next-auth/client";
 import Head from "next/head";
+import Link from "next/link";
 
 import Page from "components/pages/home";
 import { PageTitle } from "components/shared";
 
 function Home() {
+	const [session, loading] = useSession();
+
 	return (
 		<div>
 			<Head>
@@ -18,7 +22,31 @@ function Home() {
 			</Head>
 
 			<PageTitle pageTitle="Feed" />
-			<Page />
+			{/* <Page /> */}
+			<main>
+				{!session && (
+					<>
+						Not signed in <br />
+						{/* @ts-ignore */}
+						<button onClick={signIn}>Sign In</button>
+					</>
+				)}
+				{session && (
+					<>
+						Signed in as {session.user?.email} <br />
+						<div>You can now access our super secret pages</div>
+						<button>
+							<Link href="/secret">To the secret</Link>
+						</button>
+						{/* @ts-ignore */}
+						<button onClick={signOut}>sign out</button>
+						<div className="flex m-20 items-center">
+							<h1 className="font-bold text-xl mr-5">{session?.user?.name}</h1>
+							<img src={session?.user?.image || ""} width="50px" style={{ borderRadius: "50%" }} alt="avatar" />
+						</div>
+					</>
+				)}
+			</main>
 		</div>
 	);
 }
