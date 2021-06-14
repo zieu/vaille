dadf82410176d4756849d053e23e74c4cd194b4f
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-06-14 12:34:44 +0500

    changed liked page

--- src/components/pages/liked.tsx
+++ src/components/pages/liked.tsx
@@ -1,13 +1,25 @@
+import { PostCard } from "components";
+import { mock } from "services";
+
 import Link from "next/link";
 
+import { Scrollbars } from "react-custom-scrollbars";
+
 const Liked = () => {
+	let renderThumb = () => {
+		return <div className="rounded bg-primary-purple" />;
+	};
+
 	return (
-		<div className="h-full min-h-screen">
-			<h2 className="title">This is liked page</h2>
-			<Link href="/">
-				<a className="text-purple-700 underline">go to home</a>
-			</Link>
-		</div>
+		<Scrollbars universal style={{ height: "calc(100vh - 60px)" }} renderThumbVertical={renderThumb} thumbSize={50}>
+			<div className="h-full min-h-screen sm:pr-10 pr-0 md:w-full lg:w-[450px]">
+				<div className="p-6">
+					{mock.map((props, index) => (
+						<PostCard {...props} key={index} />
+					))}
+				</div>
+			</div>
+		</Scrollbars>
 	);
 };
 
