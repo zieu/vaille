02e8ef0a4726f5e2b617a7ffd042e0b8124b4edb
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2021-06-14 12:31:21 +0500

    added settings icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,3 @@
-export { default as Sun } from "./sun"
-export { default as Moon } from "./moon"
+export { default as Sun } from "./sun";
+export { default as Moon } from "./moon";
+export { default as Settings } from "./settings";
